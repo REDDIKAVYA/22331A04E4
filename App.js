2df100af93cf80e97ee8f3c8490337ed6590a859
REDDI KAVYA <reddykavya1675@gmail.com>
2025-07-04 13:28:50 +0530

    initial commit
    task is to develop a user friendly URL Shortener application

---
+++ App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import URLShortener from './URLShortener';
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>URL Shortener</h1>
+      <URLShortener />
+    </div>
+  );
+}
+
+export default App;
+
